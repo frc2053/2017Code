dc1c16ef8a3e8a01bacac331b861828edd4eb74d
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2017-01-19 11:05:52 -0500

    adjusted align center command

--- robot2017/src/Commands/Drive/AlignCenter.cpp
+++ robot2017/src/Commands/Drive/AlignCenter.cpp
@@ -25,14 +25,19 @@ void AlignCenter::Execute()
 	distanceToCenter = (320 / 2) - Robot::visionTable->GetNumber("centerX", 0.0);
 	SmartDashboard::PutNumber("distanceToCenter", distanceToCenter);
 
-	if(abs(Robot::oi->GetLeftXAxisDriver()) > .2 || abs(Robot::oi->GetLeftYAxisDriver()) > .2 || abs(Robot::oi->GetRightXAxisDriver()) > .2) {
+	std::cout << "X: " << abs(Robot::oi->GetLeftXAxisDriver()) << std::endl;
+	std::cout << "Y: " << abs(Robot::oi->GetLeftYAxisDriver()) << std::endl;
+	std::cout << "Z: " << abs(Robot::oi->GetRightXAxisDriver()) << std::endl;
+
+	if(fabs(Robot::oi->GetLeftXAxisDriver()) > .1 || fabs(Robot::oi->GetLeftYAxisDriver()) > .1 || fabs(Robot::oi->GetRightXAxisDriver()) > .1) {
 		isDone = true;
 		std::cout << "Override!" << std::endl;
 	}
 
 	speedX = CalculateSpeedValue(distanceToCenter);
 
-	Robot::drivebaseSubsystem->MecanumDrive(speedX, 0, 0, adjyaw);
+	float rot = Robot::drivebaseSubsystem->CalculateRotValue(0, 1);
+	Robot::drivebaseSubsystem->MecanumDrive(speedX, 0, rot, adjyaw);
 }
 
 // Make this return true when this Command no longer needs to run execute()
@@ -56,15 +61,16 @@ void AlignCenter::Interrupted()
 float AlignCenter::CalculateSpeedValue(int distToCenter) {
 	float returnedSpeed = 0;
 	if(distToCenter >= 15) {
-		returnedSpeed = -.25;
+		returnedSpeed = -.20;
 	}
 
 	if(distToCenter <= -15) {
-		returnedSpeed = .25;
+		returnedSpeed = .20;
 	}
 
-	if(abs(distToCenter) < 3) {
+	if(abs(distToCenter) < 1) {
 		returnedSpeed = 0;
+		isDone = true;
 	}
 
 	return returnedSpeed;
