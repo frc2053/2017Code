0aa411daaf9872997d6fd46a96203eaf87eec599
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2017-01-19 13:06:56 -0500

    cleaned up alignment command

--- robot2017/src/Commands/Drive/AlignCenter.cpp
+++ robot2017/src/Commands/Drive/AlignCenter.cpp
@@ -9,7 +9,6 @@ AlignCenter::AlignCenter()
 	speedX = 0;
 }
 
-// Called just before this Command runs the first time
 void AlignCenter::Initialize()
 {
 	distanceToCenter = 0;
@@ -18,20 +17,14 @@ void AlignCenter::Initialize()
 	speedX = 0;
 }
 
-// Called repeatedly when this Command is scheduled to run
 void AlignCenter::Execute()
 {
 	adjyaw = Robot::drivebaseSubsystem->GetAdjYaw();
 	distanceToCenter = (320 / 2) - Robot::visionTable->GetNumber("centerX", 0.0);
 	SmartDashboard::PutNumber("distanceToCenter", distanceToCenter);
 
-	std::cout << "X: " << abs(Robot::oi->GetLeftXAxisDriver()) << std::endl;
-	std::cout << "Y: " << abs(Robot::oi->GetLeftYAxisDriver()) << std::endl;
-	std::cout << "Z: " << abs(Robot::oi->GetRightXAxisDriver()) << std::endl;
-
 	if(fabs(Robot::oi->GetLeftXAxisDriver()) > .1 || fabs(Robot::oi->GetLeftYAxisDriver()) > .1 || fabs(Robot::oi->GetRightXAxisDriver()) > .1) {
 		isDone = true;
-		std::cout << "Override!" << std::endl;
 	}
 
 	speedX = CalculateSpeedValue(distanceToCenter);
@@ -40,19 +33,15 @@ void AlignCenter::Execute()
 	Robot::drivebaseSubsystem->MecanumDrive(speedX, 0, rot, adjyaw);
 }
 
-// Make this return true when this Command no longer needs to run execute()
 bool AlignCenter::IsFinished()
 {
 	return isDone;
 }
 
-// Called once after isFinished returns true
 void AlignCenter::End()
 {
 }
 
-// Called when another command which requires one or more of the same
-// subsystems is scheduled to run
 void AlignCenter::Interrupted()
 {
 
