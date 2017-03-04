00cbd65f9e19f63d99ba01f199dfbaff8115236f
Team 2053 Tigertronics <team2053tigertronics@gmail.com>
2017-03-03 19:53:25 -0500

    tuned auto modes

--- robot2017/src/Commands/Autonomous/GearAlignCenter.cpp
+++ robot2017/src/Commands/Autonomous/GearAlignCenter.cpp
@@ -10,7 +10,7 @@
 #include "../Shooter/ShooterWheel.h"
 
 GearAlignCenter::GearAlignCenter() {
-	AddSequential(new DriveCommandAuto(0, -.5, 0, 1.1, 0)); //drive forward
+	AddSequential(new DriveCommandAuto(0, -.5, 0, 1.2, 0)); //drive forward
 	//AddSequential(new AlignCenter(0)); //align
 	//AddSequential(new DriveCommandAuto(0, -.5, 0, .8, 0)); //drive forward
 	AddSequential(new DoNothingAuto(.25)); //wait
@@ -25,9 +25,14 @@ GearAlignCenter::GearAlignCenter() {
 		//AddSequential(new DriveCommandAuto(0, 0, 0, 1, -135));
 		//AddSequential(new DoNothingAuto(1));
 		AddSequential(new PrintCommand("FINISHED ROTATING"));
-		AddSequential(new DriveCommandAuto(-1, 0, 0, .5, -90));//drives to boiler
+		AddSequential(new DriveCommandAuto(0.01, 0, 0, 1, -90));//TURNS 90
+		AddSequential(new PrintCommand("FINISHED TURNING 90"));
 
-		AddSequential(new DriveCommandAuto(-.5, .5, 0, .5, -135));
+		AddSequential(new DriveCommandAuto(-1, 0, 0, .25, -90));//DRIVE SIDEWAYS
+		AddSequential(new PrintCommand("FINISHED GETTING TO THE DSBOIFS	"));
+
+
+		/*AddSequential(new DriveCommandAuto(-.5, .5, 0, .25, -135));
 		AddSequential(new PrintCommand("FINISHED BOILER 135 DRIVE COMMAND"));
 
 
@@ -39,5 +44,6 @@ GearAlignCenter::GearAlignCenter() {
 		AddSequential(new PrintCommand("END OF DRIVE COMMANDS"));
 		//AddSequential(new DriveToBoilerShootCenter()); //go to boiler
 		AddSequential(new PrintCommand("FINISHED WITH DO BOILER"));
+		*/
 	}
 }
--- robot2017/src/Commands/Autonomous/GearAlignLeft.cpp
+++ robot2017/src/Commands/Autonomous/GearAlignLeft.cpp
@@ -8,12 +8,14 @@
 #include "../Gears/RetractGearGroup.h"
 
 GearAlignLeft::GearAlignLeft() {
-	AddSequential(new DriveCommandAuto(0, -.5, 0, 1, 0)); //drive forward needs to drive farther
-	AddSequential(new AlignCenter(60)); //align with the goal
-	AddSequential(new DriveCommandAuto(0, .5, 0, .5, 60)); //drive forward onto airship
-	AddSequential(new PushGearGroup());
-	AddSequential(new DoNothingAuto(.5)); //wait
-	AddSequential(new RetractGearGroup());
+	AddSequential(new DriveCommandAuto(0, -.5, 0, .9, 0));  //drive forward needs to drive farther
+	//	AddSequential(new AlignCenter(-60)); //align with gear
+		AddSequential(new DriveCommandAuto(.25, -.25, 0, 1.5, 60)); //drive onto airship
+		AddSequential(new DoNothingAuto(.25)); //wait
+		AddSequential(new PushGearGroup());
+		AddSequential(new DoNothingAuto(.5)); //wait
+		AddSequential(new RetractGearGroup());
+		AddSequential(new DriveCommandAuto(-.25, .25, 0, .6, 60)); //drive onto airship
 	if(Robot::doBoiler) { //if we do boiler
 		if(Robot::currentAlliance == frc::DriverStation::Alliance::kBlue) {
 			AddSequential(new DriveToBoilerShootCenter()); //if on blue we go to center
